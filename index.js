caf4dd4f310c75543cd67de135274fcd2719ce85
GoHira <gohiranojp@hotmail.co.jp>
2022-04-18 10:26:17 +0900

    add 20220418 lesson03_fileops

--- index.js
+++ index.js
@@ -34,3 +34,51 @@
 // })
 // clock.start();
 
+// Lesson03-1: passモジュール
+// const path = require("path");
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(__dirname);
+// var subFold = path.join(__dirname, "modules");
+// console.log(subFold);
+// console.log(path.join(subFold, "..\\index.js"));
+
+// var homeUnnormal = subFold + "\\..\\index.js"
+// console.log(homeUnnormal);
+// console.log(path.normalize(homeUnnormal));
+
+// Lesson03-2: file reader
+// const path = require("path");
+// const fs = require("fs");
+// // variables
+// var message = "";
+// var staticPath = path.join(__dirname, "static")
+
+// // set reader stream
+// var reader = fs.createReadStream(path.join(staticPath, "sample.txt"), "utf8");
+
+// // start the stream
+// reader.on("data", (chunk) => {message += chunk;});
+// reader.on("end", () => {
+//     console.log(message);
+// });
+// reader.resume();
+
+// Lesson03-3: file writer
+// import module
+var path = require("path");
+var fs = require("fs");
+
+//variable
+var outputFileFld = path.join(__dirname, "static")
+
+// simple write method
+var writer = fs.createWriteStream(path.join(outputFileFld, "created.txt"), "utf8");
+writer.end("Hello World !");
+
+var reader = fs.createReadStream(path.join(outputFileFld, "sample.txt"), "utf8");
+var writer = fs.createWriteStream(path.join(outputFileFld, "sample-copy.txt"), "utf8");
+reader.pipe(writer);
+reader.resume();
